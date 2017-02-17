a037fca4633990804bcaa88efc69d9a270d80704
jkosta <jkostaras@yahoo.com>
2017-02-17 19:00:05 +0100

    Added buttons, icons, functions, computed properties etc.

--- client/src/main/java/todods/TodoDS/ViewModel.java
+++ client/src/main/java/todods/TodoDS/ViewModel.java
@@ -1,5 +1,9 @@
 package todods.TodoDS;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
+import static java.util.stream.Collectors.toList;
 import net.java.html.json.ComputedProperty;
 import net.java.html.json.Function;
 import net.java.html.json.Model;
@@ -25,6 +29,43 @@ final class ViewModel {
         taskList.getTasks().add(new Task(2, "Book venue!", 7, "11/01/2017", true, 2, "", false));
         taskList.applyBindings();
     }
+    
+        @Function
+    public static void removeTask(TaskList tasks, Task data) {
+        tasks.getTasks().remove(data);
+    }   
+    
+    @Function
+    public static void sortBy(TaskList tasks) {
+        tasks.getTasks().sort(new PriorityComparator());
+    }    
+    
+    @Function
+    public static void showCompleted(TaskList tasks) {
+        tasks.getTasks().stream().filter(Task::isCompleted).collect(toList());
+    }    
+    
+    private static class PriorityComparator implements Comparator<Task> {
+
+        @Override
+        public int compare(final Task t1, final Task t2) {
+            if (t1.getPriority() == t2.getPriority()) {
+                return 0;
+            } else if (t1.getPriority() > t2.getPriority()) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+    }
+
+    private static class DueDateComparator implements Comparator<Task> {
+
+        @Override
+        public int compare(final Task t1, final Task t2) {
+            return t1.getDueDate().compareTo(t2.getDueDate());
+        }
+    } 
 
     @Model(className = "Task", targetId = "", properties = {
         @Property(name = "id", type = int.class),
@@ -36,6 +77,36 @@ final class ViewModel {
         @Property(name = "obs", type = String.class),
         @Property(name = "completed", type = boolean.class)
     })
-    public static class TaskModel {  }
+    public static class TaskModel {  
+        @Function
+        public static void markAsCompleted(boolean completed) {
+            completed = true;
+        }
+        
+        @ComputedProperty
+        public static boolean isLate(String dueDate) {
+            if (dueDate == null || dueDate.isEmpty()) {
+                return false;
+            }
+            LocalDate dateDue = LocalDate.parse(dueDate,
+                    DateTimeFormatter.ofPattern("d/MM/yyyy"));
+            return (dateDue == null) ? false : dateDue.compareTo(LocalDate.now()) < 0;
+        }
+
+        @ComputedProperty
+        public static boolean hasAlert(String dueDate, boolean alert, int daysBefore) {
+            if (dueDate == null || dueDate.isEmpty()) {
+                return false;
+            }
+            LocalDate dateDue = LocalDate.parse(dueDate,
+                    DateTimeFormatter.ofPattern("d/MM/yyyy"));
+            if (!alert || dateDue == null) {
+                return false;
+            } else {
+                int dias = dateDue.getDayOfYear() - LocalDate.now().getDayOfYear();
+                return dias <= daysBefore;
+            }
+        }
+    }
 }
 
