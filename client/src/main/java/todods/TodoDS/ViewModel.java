a02602eda491e372bbef870d0236247b71ec3830
jkosta <jkostaras@yahoo.com>
2017-02-17 18:32:07 +0100

    Added TaskList model.

--- client/src/main/java/todods/TodoDS/ViewModel.java
+++ client/src/main/java/todods/TodoDS/ViewModel.java
@@ -6,27 +6,36 @@ import net.java.html.json.Model;
 import net.java.html.json.Property;
 import todods.TodoDS.js.Dialogs;
 
-@Model(className = "Task", targetId="", properties = {
-    @Property(name = "id", type = int.class),
-    @Property(name = "description", type = String.class),
-    @Property(name = "priority", type = int.class),
-    @Property(name = "dueDate", type = String.class),
-    @Property(name = "alert", type = boolean.class),
-    @Property(name = "daysBefore", type = int.class),
-    @Property(name = "obs", type = String.class),
-    @Property(name = "completed", type = boolean.class)
+@Model(className = "TaskList", targetId = "", properties = {
+    @Property(name = "tasks", type = Task.class, array = true)
 })
 final class ViewModel {
-    private static Task task;
+
     /**
      * Called when the page is ready.
      */
     static void onPageLoad() throws Exception {
-        task = new Task();
+        Task task = new Task();
         task.setPriority(10);
         task.setDescription("Finish article!");
         task.setAlert(true);
         task.setDueDate("10/03/2017");
-        task.applyBindings();
+        TaskList taskList = new TaskList();
+        taskList.getTasks().add(task);
+        taskList.getTasks().add(new Task(2, "Book venue!", 7, "11/01/2017", true, 2, "", false));
+        taskList.applyBindings();
     }
+
+    @Model(className = "Task", targetId = "", properties = {
+        @Property(name = "id", type = int.class),
+        @Property(name = "description", type = String.class),
+        @Property(name = "priority", type = int.class),
+        @Property(name = "dueDate", type = String.class),
+        @Property(name = "alert", type = boolean.class),
+        @Property(name = "daysBefore", type = int.class),
+        @Property(name = "obs", type = String.class),
+        @Property(name = "completed", type = boolean.class)
+    })
+    public static class TaskModel {  }
 }
+
