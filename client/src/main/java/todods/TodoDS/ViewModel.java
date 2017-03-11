9f3c9029fb440b4cfab10a622f45eef03eff22b0
jkosta <jkostaras@yahoo.com>
2017-03-11 21:50:51 +0100

    Added functionality for the checkboxes. Even though it works for showCompleted, it doesn't for sortByPriority and markAsCompleted.

--- client/src/main/java/todods/TodoDS/ViewModel.java
+++ client/src/main/java/todods/TodoDS/ViewModel.java
@@ -4,19 +4,24 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 import java.util.List;
+import java.util.ArrayList;
 import static java.util.stream.Collectors.toList;
 import net.java.html.json.ComputedProperty;
 import net.java.html.json.Function;
 import net.java.html.json.Model;
+import net.java.html.json.OnPropertyChange;
 import net.java.html.json.Property;
 import todods.TodoDS.js.Dialogs;
 
 @Model(className = "TaskList", targetId = "", properties = {
     @Property(name = "selected", type = Task.class),
     @Property(name = "edited", type = Task.class),
-    @Property(name = "tasks", type = Task.class, array = true)
+    @Property(name = "tasks", type = Task.class, array = true),
+    @Property(name = "showCompleted", type = boolean.class),
+    @Property(name = "sortByPriority", type = boolean.class)
 })
 final class ViewModel {
+    private static final List<Task> tasksList = new ArrayList<>();
 
     /**
      * Called when the page is ready.
@@ -33,6 +38,7 @@ final class ViewModel {
         taskList.setEdited(null);
         taskList.getTasks().add(task);
         taskList.getTasks().add(new Task(2, "Book venue!", 7, "01/04/2017", false, 2, "", false));
+        tasksList.addAll(taskList.getTasks());
         taskList.applyBindings();
     }
     
@@ -43,13 +49,13 @@ final class ViewModel {
     }    
     
     @Function
-    public static void edit(TaskList tasks, Task data) {
+    static void edit(TaskList tasks, Task data) {
         tasks.setSelected(data);
         tasks.setEdited(data.clone());
     }   
 
     @Function
-    public static void commit(TaskList tasks) {
+    static void commit(TaskList tasks) {
         final Task task = tasks.getEdited();
         if (task == null) return;
         final Task selectedTask = tasks.getSelected();
@@ -58,6 +64,7 @@ final class ViewModel {
         } else {
             tasks.getTasks().add(task);
         }
+        tasksList.add(task);
         tasks.setEdited(null);
     }     
     
@@ -68,12 +75,13 @@ final class ViewModel {
     }   
     
     @Function
-    public static void removeTask(TaskList tasks, Task data) {
+    static void removeTask(TaskList tasks, Task data) {
         tasks.getTasks().remove(data);
+        tasksList.remove(data);
     } 
     
     @Function 
-    public static void expiredTasks(final TaskList tasks) {
+    static void expiredTasks(final TaskList tasks) {
         Dialogs.showAlerts(listTasksWithAlert(tasks.getTasks()));
     }
     
@@ -82,9 +90,51 @@ final class ViewModel {
     }    
 
     @ComputedProperty
-    public static int numberOfTasksWithAlert(List<Task> tasks) {
+    static int numberOfTasksWithAlert(List<Task> tasks) {
         return listTasksWithAlert(tasks).size();
     }     
+    
+    @OnPropertyChange("showCompleted")
+    static void showCompleted(final TaskList tasks) {
+        tasks.getTasks().clear();
+        if (tasks.isShowCompleted()) {
+            tasks.getTasks().addAll(tasksList.stream().filter(Task::isCompleted).collect(toList()));
+        } else {
+            tasks.getTasks().addAll(tasksList);
+        }
+    }    
+    
+    @OnPropertyChange("sortByPriority")
+    static void sortBy(final TaskList tasks) {
+        tasks.getTasks().sort(tasks.isSortByPriority() ? 
+                new PriorityComparator() : new DueDateComparator());
+    }    
+    
+    private static class PriorityComparator implements Comparator<Task> {
+
+        @Override
+        public int compare(final Task t1, final Task t2) {
+            if (t1.getPriority() == t2.getPriority()) {
+                return 0;
+            } else if (t1.getPriority() > t2.getPriority()) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+    }    
+
+    private static class DueDateComparator implements Comparator<Task> {
+
+        @Override
+        public int compare(final Task t1, final Task t2) {
+            LocalDate t1DateDue = LocalDate.parse(t1.getDueDate(),
+                    DateTimeFormatter.ofPattern("d/MM/yyyy"));
+            LocalDate t2DateDue = LocalDate.parse(t2.getDueDate(),
+                    DateTimeFormatter.ofPattern("d/MM/yyyy"));
+            return t1DateDue.compareTo(t2DateDue);
+        }
+    }    
     
     @Model(className = "Task", targetId = "", properties = {
         @Property(name = "id", type = int.class),
@@ -97,9 +147,34 @@ final class ViewModel {
         @Property(name = "completed", type = boolean.class)
     })
     public static class TaskModel {  
-        @Function
-        public static void markAsCompleted(boolean completed) {
-            completed = true;
+        @OnPropertyChange("completed")
+        static void markAsCompleted(final Task task) {
+            task.setCompleted(task.isCompleted());
+        }
+        
+        @ComputedProperty
+        public static boolean isLate(String dueDate) {
+            if (dueDate == null || dueDate.isEmpty()) {
+                return false;
+            }
+            LocalDate dateDue = LocalDate.parse(dueDate,
+                    DateTimeFormatter.ofPattern("d/MM/yyyy"));
+            return (dateDue == null) ? false : dateDue.compareTo(LocalDate.now()) < 0;
+        }
+
+        @ComputedProperty
+        static boolean hasAlert(String dueDate, boolean alert, int daysBefore) {
+            if (dueDate == null || dueDate.isEmpty()) {
+                return false;
+            }
+            LocalDate dateDue = LocalDate.parse(dueDate,
+                    DateTimeFormatter.ofPattern("d/MM/yyyy"));
+            if (!alert || dateDue == null) {
+                return false;
+            } else {
+                int dias = dateDue.getDayOfYear() - LocalDate.now().getDayOfYear();
+                return dias <= daysBefore;
+            }
         }
     }
 }
