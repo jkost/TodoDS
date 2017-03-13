b3d2d901d1988422befad1150492b20cc0eed9a3
jkosta <jkostaras@yahoo.com>
2017-03-13 22:08:30 +0100

    Removed JS functionality. It can be performed natively from Java.

--- js/src/main/java/todods/TodoDS/js/Dialogs.java
+++ js/src/main/java/todods/TodoDS/js/Dialogs.java
@@ -1,7 +1,6 @@
 package todods.TodoDS.js;
 
 import net.java.html.js.JavaScriptBody;
-import java.util.List;
 
 /** Use {@link JavaScriptBody} annotation on methods to
  * directly interact with JavaScript. See
@@ -11,13 +10,5 @@ import java.util.List;
 public final class Dialogs {
     private Dialogs() {
     }
-    
-    @JavaScriptBody(
-            args = {"expiredTasks"},
-            body = "for (task in expiredTasks) {\n"
-            + " window.alert('Task: ' + task.getDescription() + '\nexpired on ' + task.getDueDate());\n"
-            + "}\n"
-    )
-    public static native void showAlerts(List expiredTasks);
     
 }
